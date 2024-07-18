f7f07ab5f56b09c901e80634a2b20ea9c5c3d805
Pavel Samoylenko <paulsamv@yandex.ru>
2024-07-18 10:46:49 +0300

    ewgmwoegjowe

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,13 +1,25 @@
-while getopts d: flag
-do
-    case "${flag}" in
-        d) directory=${OPTARG};;
-    esac
-done
+#while getopts d: flag
+#do
+#    case "${flag}" in
+#        d) directory=${OPTARG};;
+#    esac
+#done
+#
+#cd ../expo-updates-client
+#npx expo export
+#cd ../expo-updates-server
+#rm -rf updates/$directory/
+#cp -r ../expo-updates-client/dist/ updates/$directory
+#
+#node ./scripts/exportClientExpoConfig.js > updates/$directory/expoConfig.json
+
+runtimeVersion=$(node ./scripts/exportClientExpoConfig.js | jq -r '.runtimeVersion')
+directory="$runtimeVersion/$(date +%s)"
 
 cd ../expo-updates-client
-npx expo export
+npx expo export --experimental-bundle
 cd ../expo-updates-server
+mkdir -p updates/$directory/
 rm -rf updates/$directory/
 cp -r ../expo-updates-client/dist/ updates/$directory
 
